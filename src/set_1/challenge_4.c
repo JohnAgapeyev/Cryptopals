d1cf5f34dabeef1f670efb1ec16ce7863b15b78f
John Agapeyev <jagapeyev@gmail.com>
2017-12-13 15:22:10 -0800

    Completed Set 1 Challenge 4

--- src/set_1/challenge_4.c
+++ src/set_1/challenge_4.c
@@ -3,8 +3,8 @@
 #include "../common.h"
 
 int main(void) {
-    unsigned char best_input[10000];
-    memset(best_input, 0, 10000);
+    unsigned char best_input[128];
+    memset(best_input, 0, 128);
     unsigned char key = '\0';
     unsigned long max_score = 0;
     FILE *fp = fopen("4.txt", "rb");
@@ -12,15 +12,13 @@ int main(void) {
         fprintf(stderr, "4.txt could not be found\n");
         exit(EXIT_FAILURE);
     }
-    char *input = NULL;
-    size_t input_len;
-    size_t line_len;
-    while ((line_len = getline(&input, &input_len, fp)) > 0) {
-        if (line_len != 61) {
+    char input[128];
+    while (fgets(input, 128, fp)) {
+        if (strlen(input) != 61) {
             continue;
         }
-        unsigned char *raw_input = hex_decode((const unsigned char *) input, line_len - 1);
-        size_t raw_len = (line_len - 1) / 2;
+        unsigned char *raw_input = hex_decode((const unsigned char *) input, strlen(input) - 1);
+        size_t raw_len = (strlen(input) - 1) / 2;
         unsigned char test_buffer[raw_len];
 
         for (size_t i = 0; i < 128; ++i) {
@@ -30,21 +28,23 @@ int main(void) {
             if (score > max_score) {
                 max_score = score;
                 key = i;
-                memcpy(best_input, input, line_len);
-                printf("New best string: %s\n", input);
+                strcpy((char *)best_input, input);
             }
             free(result);
         }
         free(raw_input);
     }
+    best_input[strlen((char *) best_input) - 1] = '\0';
+    unsigned char *raw_best_input = hex_decode(best_input, strlen((char *) best_input));
+    size_t best_raw_len = strlen((char *) best_input) / 2;
 
-    free(input);
+    unsigned char test_buffer[best_raw_len];
+    memset(test_buffer, key, best_raw_len);
 
-    unsigned char test_buffer[61];
+    unsigned char *result = xor_buffer(raw_best_input, test_buffer, best_raw_len);
 
-    memset(test_buffer, key, 61);
-    unsigned char *result = xor_buffer(best_input, test_buffer, 60);
-    printf("Set 1 Challenge 4\tInput string: %s\tBest match: %c\tDecoded string: %s\n", input, key, result);
+    printf("Set 1 Challenge 4 Input string: %s Best match: %c Decoded string: %s\n", best_input, key, result);
 
+    free(raw_best_input);
     free(result);
 }
