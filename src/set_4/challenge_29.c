b64d3d3d2ff496f78ae6628c79d29a3b04298a04
John Agapeyev <jagapeyev@gmail.com>
2017-12-30 11:42:51 -0800

    Choosing to skip over challenge 29 and 30 due to implementation difficulties

---
+++ src/set_4/challenge_29.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "../common.h"
+
+/**
+ * This challenge is intentionally left empty.
+ * I spent several days analyzing various C implementations of both
+ * SHA1 and the solutions to this challenge.
+ * I was unable to understand those solutions sufficiently enough that
+ * I was confident I could modify their internal state as is required for this challenge.
+ * I even tried to write my own SHA1 implementation, but that failed.
+ * As such, the only way I'd have a solution to this challenge is to blatantly copy from others.
+ * Which is frankly a waste of my time, and wouldn't teach me anything.
+ * So rather than being stuck on this challenge for potentially weeks, I'm choosing to skip this and challenge 29
+ * in order to move on to the rest of the challenges.
+ */
+int main(void) {
+    return EXIT_SUCCESS;
+}
---
+++ src/set_4/challenge_30.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "../common.h"
+
+/**
+ * For an explanation on why this challenge is empty, see Challenge 28 for my reasoning.
+ */
+int main(void) {
+    return EXIT_SUCCESS;
+}
