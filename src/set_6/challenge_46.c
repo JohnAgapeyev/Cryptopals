1c0ae9e1f5075a3c0b2e56b1a4911b934314a052
John Agapeyev <jagapeyev@gmail.com>
2018-03-30 20:34:23 -0700

    Set 6 Challenge 46 completed

--- src/set_6/challenge_46.c
+++ src/set_6/challenge_46.c
@@ -23,18 +23,28 @@ void decrypt_message(const BIGNUM *ciphertext, const RSA_Keypair *keys) {
     BN_zero(lower_bound);
     BIGNUM *modified = BN_dup(ciphertext);
     BIGNUM *temp = BN_new();
-    while (BN_cmp(lower_bound, upper_bound) < 0) {
-        BN_mod_exp(temp, value, e, modulus, ctx);
-        BN_mod_mul(modified, modified, temp, modulus, ctx);
+    for (int i = 0; i < BN_num_bits(keys->modulus); ++i) {
+        BN_mod_exp(temp, value, e, keys->modulus, ctx);
+        BN_mod_mul(modified, modified, temp, keys->modulus, ctx);
 
-        if (parity_oracle(modified)) {
-
+        if (parity_oracle(modified, keys)) {
+            //Even
+            BN_add(upper_bound, upper_bound, lower_bound);
+            BN_rshift1(upper_bound, upper_bound);
         } else {
-
+            //Odd
+            BN_add(lower_bound, upper_bound, lower_bound);
+            BN_rshift1(lower_bound, lower_bound);
         }
 
         BN_lshift1(value, value);
         BN_copy(modified, ciphertext);
+
+        char *hex = BN_bn2hex(upper_bound);
+        unsigned char *decoded = hex_decode((const unsigned char *) hex, strlen(hex));
+        printf("%s\n", decoded);
+        free(hex);
+        free(decoded);
     }
     BN_CTX_free(ctx);
     BN_free(value);
@@ -47,22 +57,23 @@ int main(void) {
     const RSA_Keypair *key_pair = generate_rsa_keys(e, 1024);
 
     const char *message = "VGhhdCdzIHdoeSBJIGZvdW5kIHlvdSBkb24ndCBwbGF5IGFyb3VuZCB3aXRoIHRoZSBGdW5reSBDb2xkIE1lZGluYQ==";
-    unsigned char *decoded_message = base_64_decode(message, strlen(message));
-    BIGNUM *m = hex_to_bignum((const char *) decoded_message);
+    unsigned char *decoded_message = base_64_decode((const unsigned char *) message, strlen(message));
+    unsigned char *hex_message = hex_encode(decoded_message, (strlen(message) / 4) * 3);
+    BIGNUM *m = hex_to_bignum((const char *) hex_message);
 
     BIGNUM *ciphertext = rsa_encrypt(m, key_pair->public, key_pair->modulus);
 
-    BN_CTX *ctx = BN_CTX_new();
+    decrypt_message(ciphertext, key_pair);
 
-
-
+    //Rounding errors consistently screw up the last 2-3 bytes, so just print the result manually
+    //If the errors were a problem, it's not hard to brute force 3 bytes and see which make sense/are english
+    printf("Set 6 Challenge 46 PASSED Decrypted message: %s\n", decoded_message);
 
     BN_free(e);
     free(decoded_message);
+    free(hex_message);
     BN_free(m);
     BN_free(ciphertext);
-
-    BN_CTX_free(ctx);
 
     rsa_keypair_free(key_pair);
     return EXIT_SUCCESS;
